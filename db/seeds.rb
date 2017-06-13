47674561f1fd9a27bff023565fae14e90069f7e9
Moodiex0 <moodiex0@gmail.com>
2017-06-13 10:05:38 -0400

    Create Product db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,29 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  #Movie.create       ([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  #Character.create      (name: 'Luke', movie: movies.first)
+
+Product.create(
+  name: 'Cereal',
+  description: 'Frosted Flakes',
+  price_in_cents: 6.99
+)
+
+Product.create(
+  name: 'Milk',
+  description: 'Dairyland 2%',
+  price_in_cents: 3.99
+)
+
+Product.create(
+  name: 'Bread',
+  description: 'Multigrain',
+  price_in_cents: 4.99
+)
+
+Product.create(
+  name: 'Poultry',
+  description: 'Terriyaki Chicken',
+  price_in_cents: 10.99
+)
