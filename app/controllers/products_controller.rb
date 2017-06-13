f7499fef83549d1891eb6d03199e4ba5d987629e
Moodiex0 <moodiex0@gmail.com>
2017-06-13 14:17:23 -0400

    Create form for new product and save it to db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,19 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
 
 end
